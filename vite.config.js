fe86f847d5229e908d51a4d678bf2dfce82e8d49
shourja12 <shourja14@gmail.com>
2026-03-05 01:11:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Add this line, matching your exact GitHub repository name:
-  base: '/shourja-technologia/', 
+  
 })
